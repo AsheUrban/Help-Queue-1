ce73c280398a696e72822d9acc0e08d5276cb347
ryandefea <ryan.defea@gmail.com>
2022-09-12 13:17:21 -0500

    add reducer
    co-authored-by: Caroline Cerussi <caroceru@gmail.com>
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

---
+++ src/reducers/ticket-list-reducer.js
@@ -0,0 +1,24 @@
+import { getDefaultNormalizer } from "@testing-library/react";
+
+const reducer = (state = {}, action) => {
+  const { names, location, issue, id } = action;
+  switch (action.type) {
+    case 'ADD_TICKET':
+      return Object.assign({}, state, {
+        [id]: {
+          names: names,
+          location: location,
+          issue: issue,
+          id: id
+        }
+      });
+    case 'DELETE_TICKET':
+      let newState = { ...state };
+      delete newState[id];
+      return newState;
+      default:
+      return state;
+  }
+};
+
+export default reducer;
